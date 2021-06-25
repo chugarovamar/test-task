a427e9dfac6cfcc611c2f6bcc74f14a2b6b3287b
chugarovamar <73020198+chugarovamar@users.noreply.github.com>
2021-06-25 14:32:27 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,6 @@
+$('#input0').click(function() {
+    if($("select#input0 :selected").val() == "ru") {
+       $("select#input0").attr('style', 'background-image:url(img/image_7.png);background-repeat: no-repeat;');
+    }
+  
+   });
